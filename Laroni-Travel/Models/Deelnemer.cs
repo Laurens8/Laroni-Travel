424344a1bab9dbd0598f5eb2775ba97c2f083e42
Robin Vangeneugden <r0318322@student.thomasmore.be>
2023-06-08 22:08:09 +0200

    Aanpassingen Opleiding en ReizenView, zoek button op persoonview gefixt + in listview volledige naam tonen toegevoegd

--- Laroni-Travel/Models/Deelnemer.cs
+++ Laroni-Travel/Models/Deelnemer.cs
@@ -17,11 +17,21 @@ namespace Laroni_Travel.Models
     public partial class Deelnemer
     {
         private IUnitOfWork _unitOfWork = new UnitOfWork(new Laroni_TravelContext());
+
+        [NotMapped]
+        public ObservableCollection<Deelnemer> AantalDeelnemers
+        { get { return new ObservableCollection<Deelnemer>(_unitOfWork.DeelnemersRepo.Ophalen()); } }
+
+        [Required]
+        public bool Admin { get; set; }
+
+        //Navigatieproperty
+        public ICollection<DeelnemerGroepsreis> DeelnemerGroepsreizen { get; set; }
+
         [DatabaseGenerated(DatabaseGeneratedOption.Identity), Key()]
         public int DeelnemerId { get; set; }
 
-        [Required]
-        public bool Admin { get; set; }       
+        public ICollection<DeelnemerOpleiding> DeelnemerOpleidingen { get; set; }
 
         [Required]
         public string Email { get; set; }
@@ -34,6 +44,10 @@ namespace Laroni_Travel.Models
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime Geboortedatum { get; set; }
 
+        [NotMapped]
+        public string GeboortedatumInfo
+        { get { return Geboortedatum.ToString("dd-MM-yyyy"); } }
+
         [Required]
         public string Gemeente { get; set; }
 
@@ -44,7 +58,13 @@ namespace Laroni_Travel.Models
         public bool HoofdMonitor { get; set; }
 
         [Required]
-        public string Huisnummer { get; set; }       
+        public string Huisnummer { get; set; }
+
+        [NotMapped]
+        public int Leeftijd
+        { get { return DateTime.Now.Year - Geboortedatum.Year; } }
+
+        public ICollection<Medisch> Medische { get; set; }
 
         [Required]
         public bool Monitor { get; set; }
@@ -55,29 +75,20 @@ namespace Laroni_Travel.Models
         [Required]
         public string Straatnaam { get; set; }
 
+        [NotMapped]
+        public string totaaldeelnemers
+        { get { return "Er zijn totaal " + AantalDeelnemers.Count().ToString() + " deelnemers ingeschreven in de reisbureau"; } }
+
+        [NotMapped]
+        public string VolledigeNaam
+        { get { return $"{Voornaam} {Familienaam}"; } }
+
         [Required]
         public string Voornaam { get; set; }
 
-        [Required]
-        public bool Ziekenfonds { get; set; }
-
         public string? Wachtwoord { get; set; }
 
-        [NotMapped]
-        public int Leeftijd { get { return DateTime.Now.Year - Geboortedatum.Year; } }
-
-        [NotMapped]
-        public string GeboortedatumInfo { get { return Geboortedatum.ToString("dd-MM-yyyy"); } }
-
-        [NotMapped]
-        public ObservableCollection<Deelnemer> AantalDeelnemers { get { return new ObservableCollection<Deelnemer>(_unitOfWork.DeelnemersRepo.Ophalen()); } }
-
-        [NotMapped]
-        public string totaaldeelnemers { get { return "Er zijn totaal " + AantalDeelnemers.Count().ToString()+ " deelnemers ingeschreven in de reisbureau"; } }
-
-        //Navigatieproperty
-        public ICollection<DeelnemerGroepsreis> DeelnemerGroepsreizen { get; set; }
-        public ICollection<DeelnemerOpleiding> DeelnemerOpleidingen { get; set; }
-        public ICollection<Medisch> Medische { get; set; }
+        [Required]
+        public bool Ziekenfonds { get; set; }
     }
 }
